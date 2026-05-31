bac17368765fc67ecfe0ba1015a838eeebc7c9f4
NeoxHellcat54 <colejoseph677@gmail.com>
2026-05-31 12:38:49 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,14 @@
-const CACHE = "sissy-random-v2";
+const CACHE = "sissy-random-v3";
 const FILES = ["./","./index.html","./styles.css","./app.js","./manifest.json"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
   self.skipWaiting();
 });
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 self.addEventListener("fetch", event => {
   event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
